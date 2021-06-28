408c78b04ac6d06d5101fadc2b8aeb61e7b94c1e
cgcarlosg <cgcarlosg@hotmail.com>
2021-06-28 11:33:15 -0500

    initial commit

---
+++ src/components/trending.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+
+
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/movie/550?api_key=d32935c9ba902f453e2cff810df3712a`
+      )
+     
+
+     
+  return (
+    <div>
+
+
+
+    </div>
+  )
+
+
+export default Trending;
